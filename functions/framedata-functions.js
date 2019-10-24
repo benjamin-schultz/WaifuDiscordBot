5eb89620fdebb97209dc3493fe92534b71758f6d
benjamin-schultz <41674946+benjamin-schultz@users.noreply.github.com>
2019-10-24 21:07:52 +1100

    character checking in progress

--- functions/framedata-functions.js
+++ functions/framedata-functions.js
@@ -1,17 +1,31 @@
 const FDConfig = require("./fdconfig.json");
+const Request = require("request-promise");
 
-function framedata(message, args) {
+async function framedata(message, args) {
     
     const gameName = args.shift().toLowerCase();
 
     try { 
         var game = checkGame(gameName); 
     } catch (err) {
-        console.log(err);
+        message.channel.send(err.message);
         return;
     }
 
     console.log(game);
+
+    const charName = args.shift().toLowerCase();
+
+    var char = checkCharacter(charName, game)
+        .then( function () {
+            break;
+        })
+        .catch( function (err) {
+            message.channel.send(err.message);
+            return;
+        });
+
+    console.log(char);
 }
 
 function checkGame(gameName) {
@@ -20,7 +34,7 @@ function checkGame(gameName) {
     for (jsonGame in FDConfig.game) {
         if (jsonGame == gameName) {
             game = FDConfig.game[jsonGame];
-            break;
+            return game;
         }
     }
 
@@ -30,6 +44,51 @@ function checkGame(gameName) {
     }
 
     return game;
+}
+
+async function checkCharacter(charName, game) {
+    var char = null;
+
+    for (jsonChar in game.characters) {
+        if (jsonChar == charName) {
+            
+            char = game.characters[jsonChar];
+            return char;
+        }
+    }
+
+    let charURI = 'http://dustloop.com/wiki/api.php?action=parse&page=BlazBlue%20Cross%20Tag%20Battle&prop=links&format=json';
+    var result = await Request(charURI);
+    var jsonLinks = JSON.parse(result);
+    chars = [];
+    for (index in jsonLinks.parse.links) {
+        var url = jsonLinks.parse.links[index]['*'];
+        url = url.split('/');
+        if (url.length > 2) {
+            continue;
+        }
+        if (url[0] == game.urlname) {
+            name = url[1].replace(/\s+/g, '');
+            if (name.toLowerCase().includes(charName)) {
+                chars.push(name);
+            }
+        }
+    }
+
+    if (chars.length > 1) {
+        var err = new Error("Multiple possible characters, Did you mean?: " + chars.join(', '));
+        throw err;
+    } else if (chars.length == 1) {
+        console.log(chars[0]);
+        return chars[0];
+    }
+
+    if (char == null) {
+        var err = new Error("Character name unrecognized");
+        throw err;
+    }
+
+    return char;
 }
 
 module.exports = {
